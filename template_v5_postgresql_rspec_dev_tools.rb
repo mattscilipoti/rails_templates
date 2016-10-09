536f3a1b5c6071b39d5d274e6ac833b21e651260
Matt Scilipoti <matt@scilipoti.name>
2016-10-09 11:57:38 -0400

    rails 5: add devise

--- template_v5_postgresql_rspec_dev_tools.rb
+++ template_v5_postgresql_rspec_dev_tools.rb
@@ -46,6 +46,41 @@ add_gem 'figaro', :default do
   append_to_readme("\n## Configured via Figaro\n\nsee: https://github.com/laserlemon/figaro")
 end
 
+
+if yes?("Would you like to install Devise?")
+  gem "devise"
+  generate "devise:install"
+
+  # follow devise configuration instructions
+  environment %q(default_url_options = ENV.fetch('default_url_options', {host: 'localhost', port: 3000})), env: 'development'
+  environment %q(config.action_mailer.default_url_options = default_url_options), env: 'development'
+  environment %q(fail("TODO: Configure default_url_options.")), env: 'production'
+  environment %q(config.action_mailer.default_url_options = {host: "http://yourwebsite.example.com"}), env: 'production'
+
+  route "#TODO: set root route (required by Devise)"
+  route "root to: 'home#index' # req'd by devise"
+
+  file 'app/views/layouts/_flash_messages.html.haml', %q(
+    fail('TODO: render this partial in app/views/layouts/application.html.haml')
+    -# TODO: update for bootstrap, dismissable
+    -#   An example: https://gist.github.com/roberto/3344628
+    %section#flashMessages.row
+      -flash.each do |key, value|
+        %p.flash{class: "alert-#{key}"}= value
+  )
+
+  git add: "."
+  git commit: %Q{ -m "Install/configure devise environment." }
+
+  model_name = ask("What would you like the user model to be called? [User]")
+  model_name = "User" if model_name.blank?
+  generate "devise", model_name
+  rails_command "db:migrate"
+
+  git add: "."
+  git commit: %Q{ -m "Configure devise for #{model_name}" }
+end
+
 after_bundle do
   git add: "."
   git commit: %Q{ -m 'Template complete.' }
