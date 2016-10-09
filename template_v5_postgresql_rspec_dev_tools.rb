ceb611ae1c249522561305f206b6b83670b10143
Matt Scilipoti <matt@scilipoti.name>
2016-10-09 11:54:40 -0400

    rails 5: add haml-rails, supports block

--- template_v5_postgresql_rspec_dev_tools.rb
+++ template_v5_postgresql_rspec_dev_tools.rb
@@ -2,6 +2,7 @@ git :init
 git add: "."
 git commit: %Q{ -m 'Initial commit, after app ' }
 
+# adds AND commits the gem, including generator commands
 def add_gem(gem_name, groups, generator_command=nil, &block)
   message = "Added #{gem_name}"
   case groups
@@ -15,13 +16,16 @@ def add_gem(gem_name, groups, generator_command=nil, &block)
     generate generator_command
   end
 
+  yield if block_given?
+
   git add: "."
   git commit: %Q{ -m "#{message}" }
 end
 
-def add_gem_with_query(gem_name, groups, generator_command=nil)
+# asks if user wants to install the gem
+def add_gem_with_query(gem_name, groups, generator_command=nil, &block)
   if yes?("Would you like to install #{gem_name}?")
-    add_gem(gem_name, groups, generator_command)
+    add_gem(gem_name, groups, generator_command, &block)
   end
 end
 
@@ -32,6 +36,10 @@ end
 add_gem('rspec-rails', [:development, :test], 'rspec:install')
 
 # gem "cucumber-rails"
+
+add_gem('haml-rails', :default, 'haml:application_layout') do
+  run "rm 'app/views/layouts/application.html.erb'"
+end
 
 after_bundle do
   git add: "."
